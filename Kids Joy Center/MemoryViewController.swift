8981ba781632c1c55429cd4696502f09109d1265
Mit Amin <aminm2@vcu.edu>
2018-03-27 16:16:43 -0400

    cleaned code and fixed 5x4 issue

--- Kids Joy Center/MemoryViewController.swift
+++ Kids Joy Center/MemoryViewController.swift
@@ -7,11 +7,8 @@
 //
 
 import UIKit
-struct arra {
-    let mvc = MemoryViewController()
-    if
-    static var imgs = [1,2,3,4,5,6,1,2,3,4,5,6 ]
-
+class arra {
+    static var imgs = [1,2,3,4,5,6,1,2,3,4,5,6]
 }
 
 class MemoryViewController: UIViewController {
@@ -19,55 +16,56 @@ class MemoryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(difficulty!,"how hard")
-        
+        var dif : Array<String> = []
+
+        if difficulty == 1 {
+            dif = ["1", "2", "3"]
+        }
+        else if difficulty == 2 {
+            dif = ["1", "2", "3", "4"]
+            arra.imgs = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
+        }
+        else if difficulty == 3 {
+            dif = ["1", "2", "3", "4", "5"]
+            arra.imgs = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
+        }
+
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
 
-        let stackView1 = UIStackView(arrangedSubviews: createButtons( named: "1", "2", "3"))
-        stackView1.translatesAutoresizingMaskIntoConstraints = false
-        stackView1.axis = .vertical
-        stackView1.spacing = 20
-        stackView1.distribution = .fillEqually
-        
-        let stackView2 = UIStackView(arrangedSubviews: createButtons( named: "1", "2", "3") )
-        stackView2.translatesAutoresizingMaskIntoConstraints = false
-        stackView2.axis = .vertical
-        stackView2.spacing = 20
-        stackView2.distribution = .fillEqually
-        
-        let stackView3 = UIStackView(arrangedSubviews: createButtons( named: "1", "2", "3") )
-        stackView3.translatesAutoresizingMaskIntoConstraints = false
-        stackView3.axis = .vertical
-        stackView3.spacing = 20
-        stackView3.distribution = .fillEqually
-        
-        let stackView4 = UIStackView(arrangedSubviews: createButtons(named: "1", "2", "3") )
-        stackView4.translatesAutoresizingMaskIntoConstraints = false
-        stackView4.axis = .vertical
-        stackView4.spacing = 20
-        stackView4.distribution = .fillEqually
-        
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = UIStackViewDistribution.equalSpacing
-        stackView.alignment = .center
-        stackView.spacing = 20.0
-        
-        stackView.addArrangedSubview(stackView1)
-        stackView.addArrangedSubview(stackView2)
-        stackView.addArrangedSubview(stackView3)
-        stackView.addArrangedSubview(stackView4)
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
+        let stackView = createStackView(quant: dif)
         self.view.addSubview(stackView)
+
         stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+
+    }
+
+    private func createStackView(quant: [String])-> UIStackView {
+        let retStackView = UIStackView()
+        retStackView.axis = .vertical
+        retStackView.distribution = UIStackViewDistribution.equalSpacing
+        retStackView.alignment = .center
+        retStackView.spacing = 20.0
+        retStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        var i = 1
+        while (i <= 4) {
+            let stackView = UIStackView(arrangedSubviews: createButtons(named: quant))
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.axis = .horizontal
+            stackView.spacing = 20
+            stackView.distribution = .fillEqually
+            retStackView.addArrangedSubview(stackView)
+            i = i+1
+        }
+
+        return retStackView
     }
   
-    private func createButtons(named: String...) -> [UIButton] {
+    private func createButtons(named: [String]) -> [UIButton] {
         let image = UIImage(named: "question") as UIImage?
-        
         var returnArr = [UIButton]()
+
         for name in named{
             let randomIndex = Int(arc4random_uniform(UInt32(arra.imgs.count)))
             let button = UIButton()
@@ -83,41 +81,11 @@ class MemoryViewController: UIViewController {
             button.addTarget(self, action: #selector(action), for: UIControlEvents.touchUpInside)
             returnArr.append(button)
         }
-        
         return returnArr
-        
-//        return named.map{name in
-//            let button = UIButton()
-//            button.setImage(image, for: .normal)
-//            button.sizeToFit()
-//            button.widthAnchor.constraint(equalToConstant: 100).isActive = true
-//            button.heightAnchor.constraint(equalToConstant: 100).isActive = true
-//            button.translatesAutoresizingMaskIntoConstraints = false
-//            button.setTitleColor(.white, for: .normal)
-//            button.addTarget(self, action: #selector(action), for: UIControlEvents.touchUpInside)
-//            return button
-//        }
-        
     }
 
-
-
-//    @objc func buttonClicked(btn: UIButton){
-//        print("we made it")
-//        btn.alpha = 0.5
-//    }
     @objc func action(sender:UIButton!) {
         sender.setImage(nil, for: .normal)
+        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+} // end of MemoryViewController class
